982d423af20c7963289410bcc03221a0e2a49be9
DHRUVI <dhruvipatel4482@gmail.com>
2025-07-04 10:45:54 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,6 @@
+function sum (a,b){
+ return a+b
+}
+document.writeln(sum(4,5))
+
+// date of birth=10/08/2003,place=bharuch gujarat,name=dhruvi,rashi=dhanu,time=2:10p.m,gender-female,give me future and relationship stutas to july 2025 and all years
