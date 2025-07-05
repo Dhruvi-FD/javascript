953014e6e8bc602ccde28b99bc13ada9d9fa9f3f
DHRUVI <dhruvipatel4482@gmail.com>
2025-07-05 11:03:32 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,12 @@
+a = [10, 20, 30, 40, 50]
+// [20, 30, 40]
+
+var out              = a.slice(1,4);
+console.log(out);
+
+// var cities = ["delhi","mumbai","chennai"];
+// cities.splice(1,1);
+
+// console.log(cities);
+// let out = a.slice(1,3);
+// document.writeln(a)
