80a5761cd486239cacc0a08471c6b12e68ea9f94
DHRUVI <dhruvipatel4482@gmail.com>
2025-07-10 10:51:01 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,28 @@
+// 1.Create a function that takes two numbers and returns their sum.
+function addNumbers(a, b) {
+    return a + b;
+}
+
+let result = addNumbers(5, 3);
+console.log(result);
+
+// 2.Write a function that checks if a number is even or odd.
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+console.log(checkEvenOrOdd(4)); 
+console.log(checkEvenOrOdd(7));
+
+//3.Create a function to reverse a string (e.g., "hello" → "olleh").
+a = "dhruvi"
+
+console.log(reverse);
+
+//4.Make a function that returns the factorial of a number.
+
+
