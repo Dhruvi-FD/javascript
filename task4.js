81072875a880774997768ddd05a7b1159ccf19bf
DHRUVI <dhruvipatel4482@gmail.com>
2025-07-11 10:31:34 +0530

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,11 @@
+// 2.Write a function that checks if a number is even or odd.
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+console.log(checkEvenOrOdd(4)); 
+console.log(checkEvenOrOdd(7));
