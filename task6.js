bcd6a59fcad58bda15e601d109f8c3094deb961b
DHRUVI <dhruvipatel4482@gmail.com>
2025-07-14 11:05:29 +0530

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,63 @@
+// 1.Print numbers from 1 to 10
+let i = 0
+for (let i = 0; i < 11; i++) {
+    console.log(i);   
+}
+
+// 2.Print only even numbers from 1 to 20
+let a = 0
+for (let a = 0; a < 21; a++) {
+console.log(a);
+    a++;
+}
+
+// 3.Loop through an array of names and print each one
+
+
+
+// 4.Print numbers in reverse from 10 to 1
+let b = 0                                   
+for (let b = 10; b >= 1; b--) {
+    console.log(b);  
+}
+
+// 5.Add all numbers from 1 to 5 using a loop
+let v = 0
+for (let v = 0; v < 6; v++) {
+   console.log(v);  
+}
+
+// 6.Print multiplication table of 5 (up to 10)
+let k = 5
+for (let k = 1; k <= 11; k++) {
+    const h = 5*k
+
+    console.log("5 x", k ,"=" +h);
+}
+
+// 7.Print squares of numbers from 1 to 10
+let r = 1
+for (let r = 0; r <=10; r++) {
+    
+   console.log(r*r);
+}
+
+// 8.Calculate the sum of numbers from 1 to 100
+let sum = 0;
+for (let m = 1 ; m <= 100; m++) {
+    sum = sum + m;
+//  console.log("sum:",sum);
+}
+console.log("sum:",sum);
+   
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     sum = sum + i;
+// }
+// console.log("Total sum from 1 to 100 is:", sum);
+
+
+
+
+
