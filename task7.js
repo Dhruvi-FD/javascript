1129f9f4577924ca0bcf3c91890721864331ac15
DHRUVI <dhruvipatel4482@gmail.com>
2025-07-16 10:42:36 +0530

    Add files via upload

---
+++ task7.js
@@ -0,0 +1,33 @@
+// 1.Print the multiplication table of 5 (from 1 to 10)
+let i = 1;
+while (i <= 10) {
+    console.log("5 x" + i + "=" + (5 * i));
+    i++;
+    
+}
+
+// 2.Find the factorial of a given number (e.g. 5!)
+z = 0;
+while (z <= 1) {
+    console.log(z);
+}
+
+
+// 3.Print digits of a number (e.g. 1234 ➝ 1 2 3 4)
+while (a <= 0) {
+    console.log(a);                 
+    
+}
+
+
+// 4.Count the number of digits in a number
+
+
+
+// 5.Reverse a number (e.g. 123 ➝ 321)
+
+
+
+
+// 6.Count and print how many numbers from 1 to 50 are divisible by 3.
+
